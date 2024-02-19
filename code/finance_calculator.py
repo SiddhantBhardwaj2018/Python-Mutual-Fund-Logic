7fa81ebb66e8b1d5ed3ea3c6fb5cb2e55f1b96a5
SiddhantBhardwaj2018 <bhardwajsiddhant03@gmail.com>
2024-02-19 14:36:38 +0530

    Added commission calculator

--- code/finance_calculator.py
+++ code/finance_calculator.py
@@ -100,6 +100,52 @@ def calculate_required_sip_amt(current_age, retirement_age, rate, inflation, mon
     result ={"Required SIP Amount":required_sip_amt,"Required Capital":required_capital,"Monthly Required Amount":amout_required_monthly}
     return result
     
+    
+def distributor_commission_calculator(sip_amt,rate,commission_rate,time):
+    val = []
+    dict = {}
+    for i in range(time):
+        info = {}
+        info['aum'] = (12 * sip_amt) * (i + 1)
+        info['aum'] = calculate_Fv(rate / 12,(i + 1) * 12,sip_amt,0)
+        info['commission'] = (commission_rate / 100) * info['aum']
+        info['value_at_' + str(time) + "_years"] = calculate_Fv(rate,time - (i + 1),0,info['commission'])
+        val.append(info)
+    dict['val'] = val
+    dict['total_commission'] = sum([info['commission'] for info in dict['val']])
+    dict['cumulative_val_at_' + str(time) + "_years"] = sum([info['value_at_' + str(time) + "_years"] for info in dict['val']])
+    return dict
+
+def revenue_model_sip_book_size_one_time_book_size(sip_book_size,rate,commission,equity_aum,time):
+    val = []
+    for i in range(time):
+        info = {}
+        info['sip_book_size_future_value'] =  calculate_Fv(rate / 12,(i + 1) * 12,sip_book_size,0)
+        info['sip_book_size_commission'] =   (commission / 100) * info['sip_book_size_future_value']
+        info['equity_aum_future_value'] = calculate_Fv(rate,(i + 1),0,equity_aum)
+        info['equity_aum_commission'] = (commission / 100) * info['equity_aum_future_value']
+        info['gross_commission'] = info['sip_book_size_commission'] + info['equity_aum_commission']
+        val.append(info)
+    return val
+
+def difference_between_insurance_and_sip_commission(invest_amount,insurance_commission_avg,cap_appreciation_rate,trail,time):
+    val = []
+    result = {}
+    for i in range(time):
+        info = {}
+        info['insurance_paid_amount'] = insurance_commission_avg * invest_amount
+        if i == 0:
+            info['cap_appreciation'] = cap_appreciation_rate * invest_amount
+            info['cumulative_value'] = invest_amount + info['cap_appreciation']
+        else:
+            info['cap_appreciation'] = (invest_amount  + val[i - 1]['cumulative_value']) * cap_appreciation_rate
+            info['cumulative_value'] = val[i - 1]['cumulative_value'] + invest_amount + info['cap_appreciation']
+        info['upfront_tail'] = (trail / 100) * info['cumulative_value']
+        val.append(info)
+    result['val'] = val
+    result['total_insurance_commission'] = sum([info['insurance_paid_amount'] for info in result['val']])
+    result['total_upfront_trail'] = sum([info['upfront_tail'] for info in result['val']])
+    return result
 
 if __name__ == "__main__":
     val = retirement_portfolio_balance_swp_calculator(
@@ -111,4 +157,7 @@ if __name__ == "__main__":
     #print(val['final'])
     #print(calculate_total_return_with_lumpsum_and_sip(2500000,5000,0.12,10))
     #print(calculate_differential_returns_by_age(25,60,10000000,0.12))
-    print(calculate_required_sip_amt(24,60,0.12,0.04,40000,0.07,0.02,20000000))
+    #print(calculate_required_sip_amt(24,60,0.12,0.04,40000,0.07,0.02,20000000))
+    #print(distributor_commission_calculator(10000,0.12,0.75,35))
+    #print(revenue_model_sip_book_size_one_time_book_size(1250000,0.1,0.5,80000000,35))
+    print(difference_between_insurance_and_sip_commission(100000,0.225,0.12,0.5,20))
